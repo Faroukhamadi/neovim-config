ca07c8331a0ed6b783894d45768e4f3cd2504b22
faroukhamadi <faroukhamadi1@outlook.com>
2023-09-28 00:40:13 +0100

    initial commit

---
+++ lua/farouk/packer.lua
@@ -0,0 +1,69 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    })
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use {
+        'vonheikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- uncomment these if you want to manage lsp servers from neovim
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- lsp support
+            { 'neovim/nvim-lspconfig' },
+            -- autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'l3mon4d3/luasnip' },
+        }
+    }
+
+    use('lukas-reineke/lsp-format.nvim')
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    use('github/copilot.vim')
+    use('othree/html5.vim')
+    use('pangloss/vim-javascript')
+    use('evanleck/vim-svelte')
+    use('lewis6991/gitsigns.nvim')
+    use('windwp/nvim-autopairs')
+    use('windwp/nvim-ts-autotag')
+end)
