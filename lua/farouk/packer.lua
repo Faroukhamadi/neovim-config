5ca1bf5ad1bdabc3dc93096180d51a5c6758cf1b
Farouk Hamadi <36114668+Faroukhamadi@users.noreply.github.com>
2023-10-25 15:06:25 +0100

    Update packer.lua

--- lua/farouk/packer.lua
+++ lua/farouk/packer.lua
@@ -66,5 +66,7 @@ return require('packer').startup(function(use)
     use('lewis6991/gitsigns.nvim')
     use('windwp/nvim-autopairs')
     use('windwp/nvim-ts-autotag')
+    use('nvim-lua/plenary.nvim')
+    use('nvim-pack/nvim-spectre')
 end)
 
