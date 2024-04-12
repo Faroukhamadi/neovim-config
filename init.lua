6b4515f1d5db19c9ab327ca288b53b0fb10754a0
faroukhamadi <faroukhamadi1@outlook.com>
2024-04-12 14:44:04 +0100

    add highlight when yanking

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 require("farouk")
 
+-- run a vim command inside a block
+vim.cmd([[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
+augroup END
+]])
